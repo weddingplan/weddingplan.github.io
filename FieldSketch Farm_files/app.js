4b841b0b7d1c824cfb28317100a6822d84d7e1c3
Paul Lowndes <github@gtcode.com>
2018-06-12 10:20:58 -1000

    fix desktop css

--- FieldSketch Farm_files/app.js
+++ FieldSketch Farm_files/app.js
@@ -12696,7 +12696,7 @@ function reset_all_images() {
 
 function show_stage() {
 	var random = Math.floor(Math.random() * 100000 + 1) / 100;
-	(0, _jquery2.default)("span.running-total").html(" Running Total: $" + random);
+	(0, _jquery2.default)("span.running-total").html("Total: $" + random);
 	if (stage > maxStage) {
 		maxStage = stage;
 	}
