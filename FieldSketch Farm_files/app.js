6b44f97a24826d2652c291cdaa00d9729012030c
Paul Lowndes <github@gtcode.com>
2018-06-12 18:21:31 -1000

    style slider

--- FieldSketch Farm_files/app.js
+++ FieldSketch Farm_files/app.js
@@ -12653,7 +12653,58 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
 require('viewport-units-buggyfill').init();
+
+//https://raw.githubusercontent.com/PaulSpr/jQuery-Flex-Vertical-Center/master/jquery.flexverticalcenter.js
+
+
+// $.fn.flexVerticalCenter = function( options ) {
+//   var settings = $.extend({
+//     cssAttribute:   'margin-top', // the attribute to apply the calculated value to
+//     verticalOffset: 0,            // the number of pixels to offset the vertical alignment by
+//     parentSelector: null,         // a selector representing the parent to vertically center this element within
+//     debounceTimeout: 25,          // a default debounce timeout in milliseconds
+//     deferTilWindowLoad: false     // if true, nothing will take effect until the $(window).load event
+//   }, options || {});
+
+//   return this.each(function(){
+//     var $this   = $(this); // store the object
+//     var debounce;
+
+//     // recalculate the distance to the top of the element to keep it centered
+//     var resizer = function () {
+
+//       var parentHeight = (settings.parentSelector && $this.parents(settings.parentSelector).length) ?
+//         $this.parents(settings.parentSelector).first().height() : $this.parent().height();
+
+//       $this.css(
+//         settings.cssAttribute, ( ( ( parentHeight - $this.height() ) / 2 ) + parseInt(settings.verticalOffset) )
+//       );
+//       if (settings.complete !== undefined) {
+//        settings.complete();
+//       }
+//     };
+
+//     // Call on resize. Opera debounces their resize by default.
+//     $(window).resize(function () {
+//         clearTimeout(debounce);
+//         debounce = setTimeout(resizer, settings.debounceTimeout);
+//     });
+
+//     if (!settings.deferTilWindowLoad) {
+//       // call it once, immediately.
+//       resizer();
+//     }
+
+//     // Call again to set after window (frames, images, etc) loads.
+//     $(window).on('load', function () {
+//         resizer();
+//     });
+
+//   });
+
+// };
 
 var stage = 1;
 
@@ -12702,7 +12753,9 @@ function show_stage() {
 	}
 	(0, _jquery2.default)('div.question').hide();
 	reset_all_images();
-	(0, _jquery2.default)('div#panel_' + stage).show();
+	(0, _jquery2.default)('div#panel_' + stage)
+	// .flexVerticalCenter()
+	.show();
 }
 
 (0, _jquery2.default)("#stage-select").change(function (e) {
@@ -12753,6 +12806,23 @@ function show_stage() {
 (0, _jquery2.default)('#modal-background').click(function () {
 	(0, _jquery2.default)("#modal").removeClass('is-active');
 });
+
+});
+
+require.register("js/jquery.flexverticalcenter.js", function(exports, require, module) {
+"use strict";
+
+/*global jQuery */
+/*!
+* FlexVerticalCenter.js 1.0
+*
+* Copyright 2011, Paul Sprangers http://paulsprangers.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Date: Fri Oct 28 19:12:00 2011 +0100
+*/
+(function ($) {})(jQuery);
 
 });
 
